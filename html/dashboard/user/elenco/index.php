793de60c251d35ba5e6ca4543aa52ff85c3e4d2e
EmaApo <168439000+EmaApo@users.noreply.github.com>
2024-04-29 17:40:51 +0200

    Add files via upload

---
+++ html/dashboard/user/elenco/index.php
@@ -0,0 +1,33 @@
+<?php 
+    /**
+     * implementare la parte di codice che permette di viisualizzare le prenotazioni effettuate
+     */session_start();
+?>
+<html>
+    <head>
+        <title>
+            Le tue prenotazioni
+        </title>
+        <link rel="stylesheet" href="../../../styleIndex.css">
+    </head>
+    <body>
+            <section class="header">
+                <a href="index.php" class="logo">Food</a>
+
+                <nav class="navbar" style='margin-left: 20px;'>
+                    <a href="chiSiamo.php">Chi siamo</a>
+                    <a href="map.php">Map</a>
+                    <a href="#">Recensioni</a>
+                </nav>
+                <nav class="navbar">
+                    <a href="account/logout/index.php">Logout</a>
+                </nav>
+            </section>
+        <center>
+            <h1 style="margin-top: 20px;">Le tue prenotazioni</h1>
+            <div>
+                
+            </div>
+        </center>
+    </body>
+</html>
