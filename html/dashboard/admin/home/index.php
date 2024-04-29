500f608b0acb6d8be420d71bc3862c838a036b09
EmaApo <168439000+EmaApo@users.noreply.github.com>
2024-04-29 17:38:34 +0200

    Update index.php

---
+++ html/dashboard/admin/home/index.php
@@ -0,0 +1,69 @@
+<?php 
+/**
+ * collegamenti da fare:
+ * 
+ */
+?>
+<hmtl>
+    <head>
+        <title>
+            Area riservata
+        </title>
+        <link rel="stylesheet" href="../../../styleIndex.css">
+    </head>
+    <body>
+        
+        <section class="header">
+            <a href="index.php" class="logo">Food</a>
+
+            <nav class="navbar" style='margin-left: 20px;'>
+                <a href="chiSiamo.php">Chi siamo</a>
+                <a href="map.php">Map</a>
+                <a href="#">Recensioni</a>
+            </nav>
+            <nav class="navbar">
+                <a href="account/logout/index.php">Logout</a>
+            </nav>
+        </section>
+        <section class="home" id="home">
+            <center>
+            <h1 style="font-size: 36px; margin-top: 50px;">Area riservata</h1>
+                <div class="divAreaRis">
+                    <table style="margin-left: 20px; margin-rigth: 20px;">
+                        <tr>
+                            <td>
+                                <a href="../../../account\set\index.php">
+                                    <img src="../../../images/modificautente.png">
+                                </a>
+                            </td>
+                            <td>
+                                <p>Modifica profilo</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="#">
+                                    <img src="../../../images/elencoPrenotazioni.png" style='width: 60px; heigth: 60px'>
+                                </a>
+                            </td>
+                            <td>
+                                <p>Elenco prenotazioni</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="#">
+                                    <img src="../../../images/recensioni.png">
+                                </a>
+                            </td>
+                            <td>
+                                <p>Visualizza le recensioni del ristorante</p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                
+            </center>
+        </section>
+    </body>
+</hmtl>
