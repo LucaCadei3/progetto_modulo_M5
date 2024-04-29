592472faa98f5cef8c32ad422e498828093b57ef
EmaApo <168439000+EmaApo@users.noreply.github.com>
2024-04-29 16:02:11 +0200

    Add files via upload

---
+++ html/account/set/index.php
@@ -0,0 +1,23 @@
+<?php 
+    /**
+     * implementare la parte back-end
+     * 
+     */
+?>
+<html>
+    <head>
+        <title>
+            Modifica i tuoi dati
+        </title>
+    </head>
+    <body>
+        <form>
+            Nome<input type="text" name="nome" value="valori prelevati dal database">
+            <br>
+            Cognome<input type="text" name="cognome" value="valori prelevati dal database"><br>
+            Data Nascita<input type="date" name="data_nascita"><br>
+            Telefono<input type="number" name="telefono" value="valori prelevati dal database"><br>
+            Email<input type="email" name="email" value="valori prelevati dal database"><br>
+        </form>
+    </body>
+</html>
