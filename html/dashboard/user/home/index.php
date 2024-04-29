08d444dc20ca65cadfa35f35f6f36e7e99fb9e6c
EmaApo <168439000+EmaApo@users.noreply.github.com>
2024-04-29 17:39:01 +0200

    Update index.php

--- html/dashboard/user/home/index.php
+++ html/dashboard/user/home/index.php
@@ -1,86 +1,97 @@
-<?php 
-session_start();
-?>
-<hmtl>
-    <head>
-        <title>
-            Area riservata
-        </title>
-        <link rel="stylesheet" href="../../../styleIndex.css">
-    </head>
-    <body>
-        
-        <section class="header">
-            <a href="index.php" class="logo">Food</a>
+    <?php 
 
-            <nav class="navbar" style='margin-left: 20px;'>
-                <a href="chiSiamo.php">Chi siamo</a>
-                <a href="map.php">Map</a>
-                <a href="#">Recensioni</a>
-            </nav>
-            <nav class="navbar">
-                <a href="account/logout/index.php">Logout</a>
-            </nav>
-        </section>
-        <section class="home" id="home">
-            <center>
-            <h1 style="font-size: 36px; margin-top: 50x;">Area riservata</h1>
-                <div class="divAreaRis">
-                    <table style="margin-left: 20px; margin-rigth: 20px;">
-                        <tr>
-                            <td>
-                                <a href="../../../account\set\index.php">
-                                    <img src="../../../images/modificautente.png">
-                                </a>
-                            </td>
-                            <td>
-                                <p>Modifica profilo</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <a href="#">
-                                    <img src="../../../images/prenotazione.png" style='width: 60px; heigth: 60px'>
-                                </a>
-                            </td>
-                            <td>
-                                <p>Prenota un ristorante</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <a href="#">
-                                    <img src="../../../images/elencoPrenotazioni.png" style='width: 60px; heigth: 60px'>
-                                </a>
-                            </td>
-                            <td>
-                                <p>Elenco prenotazioni</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <a href="#">
-                                    <img src="../../../images/recensione.png">
-                                </a></td>
-                            <td>
-                                <p>Inserisci una recensione</p>
-                            </td>
-                            
-                        </tr>
-                        <tr>
-                            <td>
-                                <a href="#">
-                                    <img src="../../../images/recensioni.png">
-                                </a>
-                            </td>
-                            <td>
-                                <p>Visualizza le tue recensioni</p>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                
-            </center>
-        </section>
-    </body>
-</hmtl>
+    ?>
+    <hmtl>
+        <head>
+            <title>
+                Area riservata
+            </title>
+            <link rel="stylesheet" href="../../../styleIndex.css">
+        </head>
+        <body>
+            
+            <section class="header">
+                <a href="index.php" class="logo">Food</a>
+
+                <nav class="navbar" style='margin-left: 20px;'>
+                    <a href="chiSiamo.php">Chi siamo</a>
+                    <a href="map.php">Map</a>
+                    <a href="#">Recensioni</a>
+                </nav>
+                <nav class="navbar">
+                    <a href="account/logout/index.php">Logout</a>
+                </nav>
+            </section>
+            <section class="home" id="home">
+                <center>
+                <h1 style="font-size: 36px; margin-top: 50px;">Area riservata</h1>
+                    <div class="divAreaRis">
+                        <table style="margin-left: 20px; margin-rigth: 20px;">
+                            <tr>
+                                <td>
+                                    <a href="../../../account\set\index.php">
+                                        <img src="../../../images/modificautente.png">
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="../../../account\set\index.php">
+                                        <p>Modifica profilo</p>
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="../prenota/">
+                                        <img src="../../../images/prenotazione.png" style='width: 60px; heigth: 60px'>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="../prenota/">
+                                        <p>Prenota un ristorante</p>
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="../elenco/">
+                                        <img src="../../../images/elencoPrenotazioni.png" style='width: 60px; heigth: 60px'>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="../elenco/">
+                                        <p>Elenco prenotazioni</p>
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">
+                                        <img src="../../../images/recensione.png">
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#">
+                                        <p>Inserisci una recensione</p>
+                                    </a>
+                                </td>
+                                
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">
+                                        <img src="../../../images/recensioni.png">
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#">
+                                        <p>Visualizza le tue recensioni</p>
+                                    </a>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    
+                </center>
+            </section>
+        </body>
+    </hmtl>
