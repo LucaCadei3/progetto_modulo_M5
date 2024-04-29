3d39078212693c5756a67c11c92f695675669264
FilippoRodari1 <162608981+FilippoRodari1@users.noreply.github.com>
2024-04-29 17:18:30 +0200

    Add files via upload

---
+++ html/recensioni.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleIndex.css" type="text/css">
+    <title>Recensioni</title>
+</head>
+<body>
+<section class="header">
+        <a href="index.php" class="logo">Food</a>
+
+        <nav class="navbar">
+            <a href="chiSiamo.php">Chi siamo</a>
+            <a href="map.php">Map</a>
+            <a href="recensioni.php">Recensioni</a>
+        </nav>
+        <nav class="navbar">
+            <a href="account/login/index.php">Login</a>
+        </nav>
+    </section>
+
+    <br>
+    <br>
+    <br>
+    <center>
+        <div class="menu-title">RECENSIONI UTENTI</div>
+        <div class="restaurant-container">
+            <div class="box-container1" style="width: 100%;">
+                <div class="box1" style="background-color: lightyellow; width: 100%;">
+                    <div class="content1" style="border: 2px solid black; padding: 20px;">
+                        <center><img src="images/persona1.jpeg" style="width : 200px; height: 200px;" />
+                        <h3 style="font-size: 24px;">Marco93</h3>
+                        <p style="font-size: 30px;">⭐⭐⭐⭐⭐</p>
+                        <p style="font-size: 18px;">Recensione: "Sito fantastico! Facile da usare e mi ha aiutato a trovare il ristorante perfetto per la mia serata speciale. Consigliatissimo!"</p>
+                    </div></center>
+                </div>
+                <br>
+                <br>
+                <br>
+                <div class="box1" style="background-color: lightyellow; width: 100%;">
+                    <div class="content1" style="border: 2px solid black; padding: 20px;">
+                        <center><img src="images/persona2.jpeg" style="width : 200px; height: 200px;" />
+                        <h3 style="font-size: 24px;">Gastronauta87</h3>
+                        <p style="font-size: 30px;">⭐⭐⭐</p>
+                        <p style="font-size: 18px;">Recensione: "Ho trovato il mio nuovo ristorante preferito grazie a questo sito! La varietà di opzioni è incredibile e le recensioni degli altri utenti sono molto utili."</p>
+                    </div>
+                </div></center>
+                <br>
+                <br>
+                <br>
+                <div class="box1" style="background-color: lightyellow; width: 100%;">
+                    <div class="content1" style="border: 2px solid black; padding: 20px;">
+                        <center><img src="images/persona3.jpeg" style="width : 200px; height: 200px;" />
+                        <h3 style="font-size: 24px;">CucinareInsieme</h3>
+                        <p style="font-size: 30px;">⭐⭐⭐⭐</p>
+                        <p style="font-size: 18px;">Recensione: "Bellissima esperienza! Ho prenotato un tavolo per la mia famiglia e abbiamo trascorso una serata indimenticabile. Sicuramente torneremo su questo sito per le nostre prossime uscite."</p>
+                    </div>
+                </div></center>
+                <br>
+                <br>
+                <br>
+                <div class="box1" style="background-color: lightyellow; width: 100%;">
+                    <div class="content1" style="border: 2px solid black; padding: 20px;">  
+                        <center><img src="images/persona4.jpeg" style="width : 200px; height: 200px;" />
+                        <h3 style="font-size: 24px;">FoodLover99</h3>
+                        <p style="font-size: 30px;">⭐⭐⭐⭐</p>
+                        <p style="font-size: 18px;">Recensione: "Ottimo servizio e cibo delizioso! Ho trascorso una serata indimenticabile grazie a questo ristorante. Lo consiglio vivamente a tutti gli amanti del buon cibo!"</p>
+                    </div></center>
+                </div>
+                <br>
+                <br>
+                <br>
+                <div class="box1" style="background-color: lightyellow; width: 100%;">
+                    <div class="content1" style="border: 2px solid black; padding: 20px;">
+                        <center><img src="images/persona5.jpeg" style="width : 200px; height: 200px;" />
+                        <h3 style="font-size: 24px;">GourmetTraveler</h3>
+                        <p style="font-size: 30px;">⭐⭐⭐⭐⭐</p>
+                        <p style="font-size: 18px;">Recensione: "Una vera esperienza culinaria! Sono rimasto stupefatto dalla qualità dei piatti e dal servizio impeccabile. Tornerò sicuramente per assaggiare altre prelibatezze!"</p>
+                    </div></center>
+                </div>
+                <br>
+                <br>
+                <br>
+                <div class="box1" style="background-color: lightyellow; width: 100%;">
+                    <div class="content1" style="border: 2px solid black; padding: 20px;">
+                        <center><img src="images/persona5.jpeg" style="width : 200px; height: 200px;" />
+                        <h3 style="font-size: 24px;">GourmetTraveler</h3>
+                        <p style="font-size: 30px;">⭐⭐⭐⭐⭐</p>
+                        <p style="font-size: 18px;">Recensione: "Una vera esperienza culinaria! Sono rimasto stupefatto dalla qualità dei piatti e dal servizio impeccabile. Tornerò sicuramente per assaggiare altre prelibatezze!"</p>
+                    </div></center>
+                </div>
+                <br>
+                <br>
+                <br>
+                <div class="box1" style="background-color: lightyellow; width: 100%;">
+                    <div class="content1" style="border: 2px solid black; padding: 20px;">
+                        <center><img src="images/persona5.jpeg" style="width : 200px; height: 200px;" />
+                        <h3 style="font-size: 24px;">GourmetTraveler</h3>
+                        <p style="font-size: 30px;">⭐⭐⭐⭐⭐</p>
+                        <p style="font-size: 18px;">Recensione: "Una vera esperienza culinaria! Sono rimasto stupefatto dalla qualità dei piatti e dal servizio impeccabile. Tornerò sicuramente per assaggiare altre prelibatezze!"</p>
+                    </div></center>
+                </div>
+                <br>
+                <br>
+                <br>
+                <div class="box1" style="background-color: lightyellow; width: 100%;">
+                    <div class="content1" style="border: 2px solid black; padding: 20px;">
+                        <center><img src="images/persona5.jpeg" style="width : 200px; height: 200px;" />
+                        <h3 style="font-size: 24px;">GourmetTraveler</h3>
+                        <p style="font-size: 30px;">⭐⭐⭐⭐⭐</p>
+                        <p style="font-size: 18px;">Recensione: "Una vera esperienza culinaria! Sono rimasto stupefatto dalla qualità dei piatti e dal servizio impeccabile. Tornerò sicuramente per assaggiare altre prelibatezze!"</p>
+                    </div></center>
+                </div>
+                <br>
+                <br>
+                <br>
+                <div class="box1" style="background-color: lightyellow; width: 100%;">
+                    <div class="content1" style="border: 2px solid black; padding: 20px;">
+                        <center><img src="images/persona5.jpeg" style="width : 200px; height: 200px;" />
+                        <h3 style="font-size: 24px;">GourmetTraveler</h3>
+                        <p style="font-size: 30px;">⭐⭐⭐⭐⭐</p>
+                        <p style="font-size: 18px;">Recensione: "Una vera esperienza culinaria! Sono rimasto stupefatto dalla qualità dei piatti e dal servizio impeccabile. Tornerò sicuramente per assaggiare altre prelibatezze!"</p>
+                    </div></center>
+                </div>
+                <br>
+                <br>
+                <br>
+                <div class="box1" style="background-color: lightyellow; width: 100%;">
+                    <div class="content1" style="border: 2px solid black; padding: 20px;">
+                        <center><img src="images/persona5.jpeg" style="width : 200px; height: 200px;" />
+                        <h3 style="font-size: 24px;">GourmetTraveler</h3>
+                        <p style="font-size: 30px;">⭐⭐⭐⭐⭐</p>
+                        <p style="font-size: 18px;">Recensione: "Una vera esperienza culinaria! Sono rimasto stupefatto dalla qualità dei piatti e dal servizio impeccabile. Tornerò sicuramente per assaggiare altre prelibatezze!"</p>
+                    </div></center>
+                </div>
+                <br>
+                <br>
+                <br>
+            </div>
+        </div>
+    </center>
+
+
+    <br>
+    <br>
+    <br>
+    
+    <center>
+    <section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>LINK PAGINE</h3>
+            <a href="index.php"><i class="fas fa-angle-right"></i> food</a>
+            <a href="chiSiamo.php"><i class="fas fa-angle-right"></i>Chi siamo</a>
+            <a href="map.php"><i class="fas fa-angle-right"></i>Map</a>
+            <a href="#.php"><i class="fas fa-angle-right"></i>Recensioni</a>
+        </div>
+
+        <div class="box">
+            <h3>CONTATTI</h3>
+            <a href="#"><i class="fas fa-phone"></i> +123-456-7890</a>
+            <a href="#"><i class="fas fa-phone"></i> +111-222-3456</a>
+            <a href="#"><i class="fas fa-envelope"></i> //@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i> BERGAMO </a>   
+        </div>
+
+        <div class="box">
+            <h3>SEGUITECI</h3>
+            <a href="#"><i class="fab fa-facebook"></i> facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i> twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i> instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i> linkedin</a>        
+        </div>
+    </div>
+    <div class="credit"> created by <span>GRUPPO PROGETTO</span></div>
+</section>
+</center>
+</body>
+</html>
